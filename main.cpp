394be9a4bb6bdd1732f84e732f92b977636e8550
Jasmine <jazzyysuarez@gmail.com>
2025-09-14 18:24:23 -0700

    created a dynamic string array, hardcoded 5 names

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@
 #include <iostream>
 using namespace std;
 
+const int SIZE = 5;
+
 int main() {
-    
+    string *arr = nullptr;
+    arr = new string[SIZE];
+
+    *(arr + 0) = "Aaron";
+    *(arr + 1) = "Cheyenne";
+    *(arr + 2) = "Sophia";
+    *(arr + 3) = "Emily";
+    *(arr + 4) = "Jasmine";
+
+    delete [] arr;
 
     return 0;
 }
